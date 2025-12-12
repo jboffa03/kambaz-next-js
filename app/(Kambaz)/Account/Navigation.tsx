16aa441f941a1a0315bd21e2cc5b16adfd03af1f
jboffa <jboffa2003@gmail.com>
2025-12-12 01:05:09 -0500

    updating for mongodb

--- app/(Kambaz)/Account/Navigation.tsx
+++ app/(Kambaz)/Account/Navigation.tsx
@@ -8,11 +8,13 @@ import { useSelector } from "react-redux";
 
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
-  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const links = currentUser ? ["Profile", "Users"] : ["Signin", "Signup"];
  const pathname = usePathname();
  
  return (
    <Nav variant="pills">
+    {currentUser && currentUser.role === "ADMIN" && (
+       <NavLink as={Link} href={`/Account/Users`}  active={pathname.endsWith('Users')}> Users </NavLink> )}
      {links.map((link) => (
        <NavItem key={link}>
          <NavLink as={Link} href={link} active={pathname.endsWith(link.toLowerCase())}>
